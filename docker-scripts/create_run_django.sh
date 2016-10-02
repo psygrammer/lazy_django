cf06dfd6a62fbb1bc1bd9a119f2242ab8c90c778
MooSung Kim <moosungkim@MooSungui-MacBook-Pro.local>
2016-10-02 15:57:43 +0900

    Update

--- docker-scripts/create_run_django.sh
+++ docker-scripts/create_run_django.sh
@@ -1,1 +1,1 @@
-docker run -i -t --name run_django -p 8000:8000 -v [host_volume]:[container_volume] [your_account]/lazy_django:0.1 /bin/bash
+docker run -i -t --name run_django_react -p 8000:8000 -p 8080:8080 -v [host_volume]:[container_volume] [your_account]/lazy_django:0.2 /bin/bash
